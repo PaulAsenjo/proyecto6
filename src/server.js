d515454ace95230f7484618b939cf2db0d5bf50a
PaulAsenjo <paasenjo.dwfs17@bootcampudd.cl>
2025-04-08 10:37:47 -0400

    actualización

--- src/server.js
+++ src/server.js
@@ -8,9 +8,6 @@ import zapatillasRouter from "./routers/zapatillas.routes.js";
 import authRouter from "./routers/auth.routes.js";
 import { errorHandler } from "./middlewares/errorhandler.js";
 
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
 
 const app = express();
 
@@ -25,14 +22,6 @@ app.use(express.urlencoded( { extended: true}));
 
 app.use("/api/v1", zapatillasRouter);
 app.use("/api/v1/auth", authRouter);
-
-// Configuración de Swagger
-
-const __filename = new URL(import.meta.url).pathname; 
-const __dirname = path.dirname(__filename); 
-const swaggerDocument = YAML.load(path.join(__dirname, 'api.docs.yaml'));
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 
 //Middlewares de errores
 
